d68406f7364d5d18a9921741a8d2135600ed8e95
Nissi <nissiann@icloud.com>
2024-06-21 11:52:16 -0400

    Initial Commit

---
+++ Alert/AlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlertApp.swift
+//  Alert
+//
+//  Created by Nissi Sanju on 6/21/24.
+//
+
+import SwiftUI
+
+@main
+struct AlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
