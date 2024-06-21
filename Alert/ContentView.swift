606acc07958b031b7bf74e3a681f341514cc8cca
Nissi <nissiann@icloud.com>
2024-06-21 13:34:29 -0400

    changed button color (tint) to purple

--- Alert/ContentView.swift
+++ Alert/ContentView.swift
@@ -8,13 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var name = ""
+    @State private var year = ""
+    @State private var presentAlert = false
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TextField("What's your name?", text: $name)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.black, width: 2)
+            TextField("What year were you born?", text: $year)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.black, width: 2)
+                .padding()
+            Button("Submit"){
+                presentAlert = true
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(.purple)
         }
+        .alert("Information", isPresented: $presentAlert, actions: {
+        }, message: {
+            Text("Your name is \(name) \n You were born in \(year)")
+        })
         .padding()
     }
 }
